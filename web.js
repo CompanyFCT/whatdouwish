33859215808e79c2dfe74e75d70708e70cc06cbb
Fausto <fausto.alemao@gmail.com>
2013-05-16 15:20:36 -0300

    merge

--- web.js
+++ web.js
@@ -29,10 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-<<<<<<< HEAD
 app.post('/', routes.index);
-=======
->>>>>>> config project. remove all heroku template.
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
