df37a1231928f7cbf807acb1349c262587c3a11c
Fausto <fausto.alemao@gmail.com>
2013-05-20 00:11:29 -0300

    using data from db

--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,10 @@
 var Product = require('../models/product.js');
 
 exports._ = function(req, res){
-  // new Product({name: 'Camiseta VANS BOGUE', description: 'Camiseta VANS tamanho M', price: 100, oldPrice: 150}).save();
-  // console.log();
-  Product.find(function(err,docs){console.log(docs);});
-  res.render('index', { title: 'BuyMe..' });
+  new Product({name: 'Camiseta VANS BOGUE', description: 'Camiseta VANS tamanho M', price: 100, oldPrice: 150}).save();
+  console.log();
+  res.render('index', {name:'CAMISETA VANS BOGUE', description: 'CAMISETA VANS BOGUE DESC', price: '100', oldPrice: '150'});
+  /*Product.find(function(err,docs){
+    res.render('index', docs[0]);
+  });*/
 };
