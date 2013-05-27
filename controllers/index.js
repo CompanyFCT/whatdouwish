3c7a8586db1335f5a3fc538a941e1a3af7e6bf77
Fausto <fausto.alemao@gmail.com>
2013-05-27 00:08:27 -0300

    added pagination + readme + comments mongoose

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,15 @@
  * GET home page.
  */
 
-var Product = require('../models/product.js');
+// var Product = require('../models/product.js');
+var pagination = require('pagination');
 
 exports._ = function(req, res){
   // new Product({name: 'Camiseta VANS BOGUE', description: 'Camiseta VANS tamanho M', price: 100, oldPrice: 150}).save();
   // console.log();
-  res.render('index', {name:'CAMISETA VANS BOGUE', description: 'CAMISETA VANS BOGUE DESC', price: '100', oldPrice: '150'});
   // Product.find(function(err,docs){
   //   res.render('index', docs[0]);
   // });
+
+  res.render('index', {name:'CAMISETA VANS BOGUE', description: 'CAMISETA VANS BOGUE DESC', price: '100', oldPrice: '150'});
 };
