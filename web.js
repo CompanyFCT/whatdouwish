9b77c5b9d070efc0c52f7e92a3a6fcff1292c219
Fausto <fausto.alemao@gmail.com>
2013-05-28 16:16:11 -0300

    replace ejs to jade because html as like syntactic-sugar. discard stylus and using less because less doesnt have bug like stylus using selector

--- web.js
+++ web.js
@@ -10,7 +10,8 @@ var express = require('express')
   // , mongoose = require('mongoose')
   , routes = require('./controllers/routes')
   , locals = require('./base/locals').setLocals
-  , path = require('path');
+  , path = require('path')
+  , less = require('less-middleware');
 
 var app = express();
 
@@ -19,15 +20,15 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.set('facebook', {name: 'BuyMe', app_id: '516291248419238', app_secret_id: '40d466313102965fddb33b8ca44b6f9c'});
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(locals);
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(app.router);  
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
