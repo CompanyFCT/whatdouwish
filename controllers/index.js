4bb9aa051809cd958c89779820ebfe1fabeb4a87
Fausto <fausto.alemao@gmail.com>
2013-06-05 11:28:56 -0300

    trying to config mongo in heroku

--- controllers/index.js
+++ controllers/index.js
@@ -9,9 +9,7 @@ exports._ = function(req, res){
   Product.find(function(err,docs){
 
     var json = null;
-    if(docs==undefined){
-      console.log(docs);
-
+    if(docs=='undefined'){
       json = {name: 'Camiseta VANS BOGUE', description: 'Camiseta VANS tamanho M', price: 100, oldPrice: 150};
       new Product(json).save();
     }else{
