47c044ae53261683987dbdaa407596475ac93187
Fausto <fausto.alemao@gmail.com>
2013-06-05 10:56:58 -0300

    config mongolab

--- web.js
+++ web.js
@@ -49,7 +49,7 @@ if ('development' == app.get('env')) {
   // mongoose.connect('mongodb://heroku_app15611687:jbkpp#123@ds029328.mongolab.com:29328/heroku_app15611687');
 } 
 else if ('production' == app.get('env')) {
-  mongoose.connect(process.env.MONGOLAB);
+  mongoose.connect(process.env.MONGOLAB_URI);
 } 
 
 
